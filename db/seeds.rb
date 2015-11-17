6260bf22f248287b2a94d65ce67bfcd3c02ac982
Solar <s.olugebefola@gmail.com>
2015-11-17 17:41:52 -0500

    Fix React.render code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 
+# Institution.create(
+#   name: "First Pirate Bank",
+#   url: "#",
+#   logou_url:
+# )
