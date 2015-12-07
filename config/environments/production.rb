952f38ce8e1825347a8318666400dcbbab5103a4
Solar Olugebefola <s.olugebefola@gmail.com>
2015-12-07 10:09:33 -0500

    Fix for heroku image display.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.react.addons = true
 
+  config.serve_static_assets = true
+  config.assets.compile = true
+
 end
